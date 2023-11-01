99521dd743f2c1f77d0bdd978ee68177101e7250
Denis <99329524+Kolesnichenko0@users.noreply.github.com>
2023-11-01 19:53:06 +0200

    Update libmx.h

--- libmx/inc/libmx.h
+++ libmx/inc/libmx.h
@@ -101,7 +101,7 @@ void mx_push_back(t_list **list, void *data);
 void mx_pop_front(t_list **head);
 void mx_pop_back(t_list **head);
 int mx_list_size(t_list *list);
-t_list *mx_sort_list(t_list *lst, void *vertex, bool(*cmp)(void *, void *, void *));
+t_list *mx_sort_list(t_list *lst, bool(*cmp)(void *, void *));
 
 //Act: My list pack
 void mx_clear_list(t_list **list);
